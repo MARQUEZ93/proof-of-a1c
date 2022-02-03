d0468a5232e5d63e171b9120a9a0a70123f7f769
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2022-02-02 18:40:54 -0600

    basic header/layout

--- components/header.js
+++ components/header.js
@@ -1,83 +1,32 @@
-import Link from "next/link"
+import Link from "next/link";
+import { Menu } from 'semantic-ui-react';
 import { signIn, signOut, useSession } from "next-auth/react"
-import styles from "./header.module.css"
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
-  const { data: session, status } = useSession()
-  const loading = status === "loading"
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   return (
-    <header>
-      <noscript>
-        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
-      </noscript>
-      <div className={styles.signedInStatus}>
-        <p
-          className={`nojs-show ${
-            !session && loading ? styles.loading : styles.loaded
-          }`}
-        >
-          {!session && (
-            <>
-              <span className={styles.notSignedInText}>
-                You are not signed in
-              </span>
-              <a
-                href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signIn()
-                }}
-              >
-                Sign in
-              </a>
-            </>
-          )}
-          {session && (
-            <>
-              {session.user.image && (
-                <span
-                  style={{ backgroundImage: `url('${session.user.image}')` }}
-                  className={styles.avatar}
-                />
-              )}
-              <span className={styles.signedInText}>
-                <small>Signed in as</small>
-                <br />
-                <strong>{session.user.email || session.user.name}</strong>
-              </span>
-              <a
-                href={`/api/auth/signout`}
-                className={styles.button}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
-                }}
-              >
-                Sign out
-              </a>
-            </>
-          )}
-        </p>
-      </div>
-      <nav>
-        <ul className={styles.navItems}>
-          <li className={styles.navItem}>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
+        <Menu style={{ marginTop: '10px' }}>
+          <Link href="/">
+            <a className="item">Proof of A1C</a>
+          </Link>
+
+          <Menu.Menu position="right">
             <Link href="/contracts/new">
-              <a>Deploy Contract</a>
+              <a className="item">Deploy Contract</a>
             </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+
+            {!session && (<Link href="/api/auth/signin">
+              <a className="item">Connect to Dexcom</a>
+            </Link>)}
+            {session && (<Link href="/api/auth/signout">
+              <a className="item">Disconnect from Dexcom</a>
+            </Link>)}
+        </Menu.Menu>
+      </Menu>
+  );
 }
