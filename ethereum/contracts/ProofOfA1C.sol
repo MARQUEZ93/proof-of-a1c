0846037d4a9596df5cc66fdf863415b537c77adb
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2022-02-16 19:39:10 -0600

    finally got bgm data on chain

--- ethereum/contracts/ProofOfA1C.sol
+++ ethereum/contracts/ProofOfA1C.sol
@@ -7,17 +7,16 @@ contract ProofOfA1CFactory {
     address[] public deployedContracts;
     mapping(address => bool) public diabetics;
 
-    address public owner;
+    address public factoryCreator;
     string public endpoint; 
-    address public notset;
 
     constructor(string memory url) {
-        owner = msg.sender;
+        factoryCreator = msg.sender;
         endpoint = url; 
     }
     function createProofOfA1C() public {
         require(!diabetics[msg.sender]);
-        ProofOfA1C newProofOfA1C = new ProofOfA1C(msg.sender, owner, endpoint);
+        ProofOfA1C newProofOfA1C = new ProofOfA1C();
         deployedContracts.push(address(newProofOfA1C));
         diabetics[msg.sender] = true;
     }
@@ -25,7 +24,7 @@ contract ProofOfA1CFactory {
         return deployedContracts;
     }
     function destroyContract(address payable _to) public {
-        require(msg.sender == owner, "You are not the owner");
+        require(msg.sender == factoryCreator, "You are not the owner");
         selfdestruct(_to);
     }
 }
@@ -33,48 +32,49 @@ contract ProofOfA1CFactory {
 contract ProofOfA1C is ChainlinkClient {
     using Chainlink for Chainlink.Request;
     address public diabetic;
-    address public owner;
+    address public provider;
 
     uint256 public a1c;
     uint256 public lastProofOfA1C;
 
-    string public endpoint; 
+    string public api; 
 
     uint256 public amountRewarded;
     uint256 public totalDonations;
 
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
     // atm, only allow POA1C site to withdraw link & run interactions b/w 
 
-    constructor(address deployer, address factory, string memory url) {
-        diabetic = deployer;
-        owner = factory;
-        endpoint = url; 
+    constructor() {
+
         setPublicChainlinkToken();
+        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
+        jobId = "d5270d1c311941d0b08bead21fea7747";
+        fee = 0.1 * 10 ** 18; // (Varies by network and job)
     }
-    function appendFourStrings(string memory a, string memory b, string memory c, string memory d) internal pure returns (string memory) {
-        return string(abi.encodePacked(a, b, c, d));
-    }
-    function requestProofOfA1C(address _oracle, bytes32 _jobId, uint256 _payment, string memory dateString) public returns (bytes32 requestId) {
-        // TODO require lastProofOfA1C is at least 3 months older than sliced dateString
-        string memory userAddress = toAsciiString(diabetic);
-        Chainlink.Request memory request = buildChainlinkRequest(_jobId, address(this), this.fulfill.selector);
-        string memory url = appendFourStrings(endpoint, userAddress, "/", dateString);
-        request.add("get", url);
+    function concatenate(string memory a,string memory b, string memory c, string memory d) public pure returns (string memory){
+        return string(bytes.concat(bytes(a), bytes(b), bytes(c), bytes(d)));
+    } 
+    function requestProofOfA1C() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        request.add("get", "https://alejandropoa1c.free.beeceptor.com/api/a1c/0xB22978681f53A58E17f338f81B6C88524Ef3C9DA/2019-01-21T01:24:492019-02-21T01:24:49");
         // TODO require dateString is 3 months older than lastProofOfA1C
         // lastProofOfA1C = getSlice(19, dateString);
 
         request.add("path", "data.value");
-        
+
         // Multiply the result by 1000000000000000000 to remove decimals
         int timesAmount = 10**18;
-        //TODO confirm this is 0.1
         request.addInt("times", timesAmount);
         
         // Sends the request
-        return sendChainlinkRequestTo(_oracle, request, _payment);
+        return sendChainlinkRequestTo(oracle, request, fee);
     }
-    function fulfill(bytes32 _requestId, uint256 _mean) public recordChainlinkFulfillment(_requestId) {
-        a1c = _mean;
+    function fulfill(bytes32 _requestId, uint256 _value) public recordChainlinkFulfillment(_requestId) {
+        a1c = _value;
         lastProofOfA1C = block.timestamp; 
         // if (a1c < 7){
             //TODO if a1c below 7 then reqard diabetic
@@ -95,7 +95,7 @@ contract ProofOfA1C is ChainlinkClient {
         _;
     }
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == provider);
         _;
     }
     modifier onlyContract() {
@@ -115,6 +115,17 @@ contract ProofOfA1C is ChainlinkClient {
             s[2*i+1] = char(lo);            
         }
         return string(s);
+    }
+     function cancelRequest(
+        bytes32 _requestId,
+        uint256 _paymentCancel,
+        bytes4 _callbackFunctionId,
+        uint256 _expiration
+    )
+        public
+        onlyOwner
+    {
+        cancelChainlinkRequest(_requestId, _paymentCancel, _callbackFunctionId, _expiration);
     }
     function char(bytes1 b) internal pure returns (bytes1 c) {
         if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
@@ -138,13 +149,25 @@ contract ProofOfA1C is ChainlinkClient {
         require(sent, "Failed to send Ether");
         totalDonations+=msg.value;
     }
+    // todo need 2 test this
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
     }
+    
     function withdraw(address payable _to) public payable onlyOwner{
         (bool sent, bytes memory data) = _to.call{value: msg.value}("");
         require(sent, "Failed to send Link");
+    }
+     // A helper funciton to make the string a bytes32
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+        }
+        assembly { // solhint-disable-line no-inline-assembly
+        result := mload(add(source, 32))
+        }
     }
 }
 
