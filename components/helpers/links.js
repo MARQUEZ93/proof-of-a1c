c347389b2b85025a7b2d34e4b586a7c7e35490dc
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2022-04-26 20:45:47 -0500

    break up directories

---
+++ components/helpers/links.js
@@ -0,0 +1,5 @@
+export default {
+    "alejandro": "https://www.dromarquez.com/about",
+    "bryan": "https://www.linkedin.com/in/bryan-murren-a77b49b9/",
+    "academic": "https://pubmed.ncbi.nlm.nih.gov/31357725/"
+};
---
+++ components/helpers/who-descriptions.js
@@ -0,0 +1,4 @@
+export default {
+    "alejandro": "Alejandro is Type 1 Diabetic. He originated the project. He works as a software developer at a healthcare company.",
+    "bryan": "Bryan’s day job is as an architectural designer. He spends his time learning to design a more equitable future. He led the way on UX/Design.",
+};
