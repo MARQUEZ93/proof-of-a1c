8c99374a42dbbe6febd8eecfeac0bc87effe25f4
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2022-04-27 01:07:33 -0500

    set production values to launch site

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     reactStrictMode: true,
     serverRuntimeConfig: {
-        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+        secret: 'MAUlAvDioj2IXrM3gNnI6g1y3ttiSI1ci2CAk8mLaem='
     },
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://www.proofofa1c.com/api' // production api
     }
 }
