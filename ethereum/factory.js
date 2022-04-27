89b49c74021fb96fd7098700f1815bc7714405bb
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2022-04-27 01:51:27 -0500

    make deployment changes part 2

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
 import web3 from './web3';
 import ProofOfA1CFactory from './build/ProofOfA1CFactory.json';
-import { factoryAddress } from './factoryAddress';
 
 const instance = new web3.eth.Contract(
   ProofOfA1CFactory["abi"],
-  factoryAddress
+  "0xb218baD2AA889A6a4b2fcCd0D930066196FDb680"
 );
 
 export default instance;
--- ethereum/factoryAddress.js
+++
@@ -1,3 +0,0 @@
-export default {
-    factoryAddress: "0xb218baD2AA889A6a4b2fcCd0D930066196FDb680"
-};
